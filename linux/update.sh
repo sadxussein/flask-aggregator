81bb486db83397e23b312e157fe8d6f73557ec41
krasnoschekovvd <krasnoschekovvd@rncb.ru>
2025-01-15 13:37:03 +0300

    updated monitoring host data collection

--- linux/update.sh
+++ linux/update.sh
@@ -3,7 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source /app/flask-aggregator/bin/activate
-pip3 install --upgrade $SCRIPT_DIR/app/latest.whl
+LATEST_WHL=$(readlink $SCRIPT_DIR/app/latest.whl)
+echo "$LATEST_WHL"
+pip3 install --upgrade "$LATEST_WHL"
 deactivate
 
 chown -R aggregator:aggregator-group /app
--- src/flask_aggregator/back/run/monitoring/get_hosts.py
+++ src/flask_aggregator/back/run/monitoring/get_hosts.py
@@ -3,6 +3,7 @@
 Retrieves information about hosts in oVirt."""
 
 import json
+from datetime import datetime, timedelta
 
 from flask_aggregator.back.dbmanager import DBManager
 from flask_aggregator.back.models import Host
@@ -12,25 +13,32 @@ def run():
     """External runner."""
     dbmanager = DBManager()
     hosts = dbmanager.get_all_data_as_dict(Host)
-    result = []
+    latest_records = {}
     for host in hosts:
-        host_present = False        
-        for el in result:
-            if el["name"] == host["name"]:
-                host_present = True
-                el["status"] = host["status"]
-        if not host_present:
-            h = {
+        # If host info was created more than 1 day ago we ignore it.
+        if (
+            host["name"] not in latest_records or
+            host["time_created"] > latest_records[host["name"]]["time_created"]
+        ):
+            latest_records[host["name"]] = {
                 "name": host["name"],
                 "ip": host["ip"],
-                "status": host["status"]
+                "status": host["status"],
+                "time_created": host["time_created"]
             }
-            result.append(h)
-    if result:
-        with open(
-            f"{Config.ROOT_DIR}/get_hosts.json", 'w', encoding="utf-8"
-        ) as file:
-            json.dump(result, file, indent=4, ensure_ascii=False)
+    keys_to_ignore = ["time_created"]
+    host_list = list(latest_records.values())
+    result = [
+        {
+            k: v for k, v in values.items()
+            if k not in keys_to_ignore
+        }
+        for values in latest_records.values()
+    ]
+    with open(
+        f"{Config.ROOT_DIR}/get_hosts.json", 'w', encoding="utf-8"
+    ) as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     run()
